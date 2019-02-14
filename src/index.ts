4039074de251999fe32e5492d1c71ce4deff42d7
Juuso Mikkonen <juuso.mikkonen@reaktor.com>
2019-02-14 17:11:38 +0200

    Add support for multiple dice and constants in an expression

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,52 @@
 import { ApolloServer } from 'apollo-server'
 import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLNonNull, GraphQLFloat } from 'graphql'
 
-type Dice = [number, number, number]
+type Dice = [number, number]
 
-function parseDice(dice: string): Dice {
-  const [multiplier, sides, constant] = dice
-    .match(/(\d*)d(\d+)([\+|-]\d+)?/)
-    .slice(1)
-    .map(str => parseInt(str, 10))
-  return [
-    multiplier || 1,
-    sides,
-    constant || 0
-  ]
+function parseDice(inputStr: string): Dice[] {
+  const diceStrs = inputStr.match(/([+|-]?\d*d\d+)/g) || []
+  const constStrs = (inputStr.match(/([\+|-]\d+)[+|-]/g) || []).map(x => x.slice(0, -1))
+    .concat((inputStr.match(/([\+|-]\d+)$/) || []).slice(1))
+  // TODO: Add starting constant without + or - case to consts
+  const dice = diceStrs.map(ds => ds.split('d').map(d => parseInt(d, 10)) as Dice)
+  const constDice = constStrs.map(cs => [parseInt(cs, 10), 1] as Dice)
+  console.log(constDice)
+  return dice.concat(constDice)
 }
 
-function countResult(multiplier: number, sides: number, constant: number) {
-  return multiplier * Math.floor(Math.random() * sides + 1) + constant
+function countResult(dice: Dice[]) {
+  return dice
+    .map(([multiplier, sides]) => multiplier * Math.floor(Math.random() * sides + 1))
+    .reduce((a, b) => a + b, 0)
 }
 
-function calculateMean(multiplier: number, sides: number, constant: number) {
-  return multiplier * (sides + 1) / 2 + constant
+function calculateMean(dice: Dice[]) {
+  return dice
+    .map(([multiplier, sides]) => multiplier * (sides + 1) / 2)
+    .reduce((a, b) => a + b, 0)
 }
 
-function calculateMax(multiplier: number, sides: number, constant: number) {
-  return multiplier * sides + constant
-}
+// function calculateMax(multiplier: number, sides: number, constant: number) {
+//   return multiplier * sides + constant
+// }
 
-function calculateMin(multiplier: number, sides: number, constant: number) {
-  return multiplier + constant
-}
+// function calculateMin(multiplier: number, sides: number, constant: number) {
+//   return multiplier + constant
+// }
 
 const RollStatsType =  new GraphQLObjectType({
   name: 'Statistics',
   fields: {
-    average: { type: GraphQLFloat, resolve: (parent: {dice: Dice}) => calculateMean(...parent.dice) },
-    max: { type: GraphQLInt, resolve: (parent: {dice: Dice}) => calculateMax(...parent.dice)},
-    min: { type: GraphQLInt, resolve: (parent: {dice: Dice}) => calculateMin(...parent.dice)}
+    average: { type: GraphQLFloat, resolve: (parent: {roll: Dice[]}) => calculateMean(parent.roll) },
+    max: { type: GraphQLInt, resolve: (parent: {roll: Dice[]}) => calculateMean(parent.roll)},
+    min: { type: GraphQLInt, resolve: (parent: {roll: Dice[]}) => calculateMean(parent.roll)}
   }
 })
 
 const RollResultType = new GraphQLObjectType({
   name: 'Roll',
   fields: {
-    result: { type: GraphQLInt, resolve: (parent: {dice: Dice}) => countResult(...parent.dice) },
+    result: { type: GraphQLInt, resolve: (parent: {roll: Dice[]}) => countResult(parent.roll) },
     statistics: { type: RollStatsType, resolve: parent => parent }
   }
 })
@@ -61,7 +64,7 @@ const schema = new GraphQLSchema({
             type: new GraphQLNonNull(GraphQLString) 
           }
         },
-        resolve: (_, args) => ({dice: parseDice(args.dice)})
+        resolve: (_, args) => ({roll: parseDice(args.dice)})
       }
     }
   })
@@ -72,3 +75,10 @@ const server = new ApolloServer({ schema })
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 })
+
+
+// (\d*d\d+)
+
+// [\+|-]\d+
+
+// ([\+|-]\d+)[\+|-]
